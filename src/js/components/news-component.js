ce0ef3149d3759d0b85bf78b2957ab38cf19c18b
Elizaveta <eli3avetta@mail.ru>
2021-09-07 19:48:46 +0300

    блок новостей свайпер

--- src/js/components/news-component.js
+++ src/js/components/news-component.js
@@ -1,8 +1,11 @@
+import { Navigation, Swiper } from 'swiper';
+
+Swiper.use([Navigation]);
+
 export class NewsComponent {
   hostElem;
   btnPrev;
   btnNext;
-  cardElems;
 
   currentSlide = 0;
   windowIsLarge;
@@ -11,85 +14,30 @@ export class NewsComponent {
     this.hostElem = document.querySelector('#news-block-host');
     if (!this.hostElem) return;
 
+    const swiperElem = this.hostElem.querySelector('.news__swiper');
     this.btnPrev = this.hostElem.querySelector('.news__navigate-navigate-btn-wrapper.mod-prev');
     this.btnNext = this.hostElem.querySelector('.news__navigate-navigate-btn-wrapper.mod-next');
-    this.cardElems = this.hostElem.querySelectorAll('.news__card');
     this.windowIsLarge = window.innerWidth > 567;
 
-    this.updateCardsList();
+    new Swiper(swiperElem, {
+      slidesPerView: 1,
+      lazy: true,
+      keyboard: true,
+      speed: 400,
+      navigation: {
+        nextEl: '.news__navigate-navigate-btn-wrapper.mod-next',
+        prevEl: '.news__navigate-navigate-btn-wrapper.mod-prev',
+      },
 
-    this.btnPrev.onclick = () => {
-      if (this.currentSlide > 0) {
-        this.currentSlide--;
-        this.setActiveCard();
-      }
-    }
-
-    this.btnNext.onclick = () => {
-      if (this.windowIsLarge) {
-        if (this.currentSlide < (this.cardElems.length / 2) - 1) {
-          this.currentSlide++;
-          this.setActiveCard();
-        }
-      } else {
-        if (this.currentSlide < this.cardElems.length - 1) {
-          this.currentSlide++;
-          this.setActiveCard();
+      breakpoints: {
+        769: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+        1025: {
+          spaceBetween: 30
         }
       }
-    }
-
-    window.addEventListener('resize', () => {
-      this.updateCardsList();
-    });
-  }
-
-  updateCardsList() {
-    this.windowIsLarge = window.innerWidth > 567;
-    this.setActiveCard();
-  }
-
-  setActiveCard() {
-    if (this.windowIsLarge) {
-      this.cardElems.forEach((card, i) => {
-        if (i === this.currentSlide * 2 || i === this.currentSlide * 2 + 1) {
-          card.classList.add('mod-show');
-        } else {
-          card.classList.remove('mod-show');
-        }
-      })
-    } else {
-      this.cardElems.forEach((card, i) => {
-        if (i === this.currentSlide) {
-          card.classList.add('mod-show');
-        } else {
-          card.classList.remove('mod-show');
-        }
-      })
-    }
-
-    this.checkDisabledBtns();
-  }
-
-  checkDisabledBtns() {
-    if (this.currentSlide === 0) {
-      this.btnPrev.setAttribute('disabled', true);
-    } else {
-      this.btnPrev.removeAttribute('disabled');
-    }
-
-    if (this.windowIsLarge) {
-      if (this.currentSlide >= (this.cardElems.length / 2) - 1) {
-        this.btnNext.setAttribute('disabled', true);
-      } else {
-        this.btnNext.removeAttribute('disabled');
-      }
-    } else {
-      if (this.currentSlide === this.cardElems.length - 1) {
-        this.btnNext.setAttribute('disabled', true);
-      } else {
-        this.btnNext.removeAttribute('disabled');
-      }
-    }
+    })
   }
 }
