cc3e82d38636748a14dcd9f1e16793e1b38d2b33
Elizaveta <eli3avetta@mail.ru>
2021-09-08 15:22:36 +0300

    временный селект

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -2,21 +2,23 @@ export class Header {
 
   constructor() {
     const hostElem = document.querySelector('#header-host');
-    const btnBurger = hostElem.querySelector('.app-header__burger');
+    // const btnBurger = hostElem.querySelector('.app-header__burger');
     const mobileBox = document.querySelector('.mobile-box');
     const body = document.querySelector('body');
+    const selectWrapperElem = hostElem.querySelector('.header__lang-select-wrapper');
+    const selectedItemElem = hostElem.querySelector('.header__lang-selected-item');
 
     let isOpen = false;
 
-    btnBurger.onclick = () => {
-      if (!isOpen) {
-        mobileBox.classList.add('opened');
-        body.classList.add('mod-no-scroll');
-        setTimeout(() => {
-          isOpen = !isOpen;
-        })
-      }
-    }
+    // btnBurger.onclick = () => {
+    //   if (!isOpen) {
+    //     mobileBox.classList.add('opened');
+    //     body.classList.add('mod-no-scroll');
+    //     setTimeout(() => {
+    //       isOpen = !isOpen;
+    //     })
+    //   }
+    // }
 
     hostElem.onclick = () => {
       if (isOpen) {
@@ -25,6 +27,14 @@ export class Header {
         setTimeout(() => {
           isOpen = !isOpen;
         })
+      }
+    }
+
+    selectWrapperElem.onclick = () => {
+      if (selectWrapperElem.className.includes('mod-show')) {
+        selectWrapperElem.classList.remove('mod-show');
+      } else {
+        selectWrapperElem.classList.add('mod-show');
       }
     }
   }
