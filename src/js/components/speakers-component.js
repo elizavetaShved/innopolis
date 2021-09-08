18ef2ec2a7c534f9693a8f6b6114c86301f28708
Elizaveta <eli3avetta@mail.ru>
2021-09-08 11:11:11 +0300

    build

--- src/js/components/speakers-component.js
+++ src/js/components/speakers-component.js
@@ -1,11 +1,26 @@
+export const STEP_OPENED_SPEAKERS = 10;
+
 export class SpeakersComponent {
+  btnShowMore;
+  speakerItemElems;
+  openingCardCount = 0;
+  openingAvailableCount;
 
   constructor() {
     const hostElem = document.getElementById('speakers-host');
     if (!hostElem) return;
 
     const itemWrapper = hostElem.querySelectorAll('.speakers__message-container');
-    const btnShowMore = hostElem.querySelector('.speakers__btn-show-more');
+    this.btnShowMore = hostElem.querySelector('.speakers__btn-show-more');
+    this.speakerItemElems = hostElem.querySelectorAll('.speakers__item');
+
+    this.speakerItemElems.forEach(elem => {
+      if (!elem.className.includes('mod-hide')) {
+        this.openingCardCount ++;
+      }
+    })
+
+    this.setBtnText();
 
     itemWrapper.forEach(elem => {
       elem.onmouseover = () => {
@@ -14,8 +29,25 @@ export class SpeakersComponent {
       }
     });
 
-    btnShowMore.onclick = () => {
-      console.log('показать еще 20')
+    this.btnShowMore.onclick = () => {
+      console.log('открыто', this.openingCardCount)
+      console.log('всего', this.speakerItemElems.length)
+      if (this.openingCardCount < this.speakerItemElems.length) {
+        this.speakerItemElems.forEach((elem, index) => {
+          if (index - this.openingCardCount > 0 && index - this.openingCardCount <= this.openingAvailableCount) {
+            elem.classList.remove('mod-hide');
+          }
+        })
+        this.setBtnText();
+
+        this.openingCardCount += this.openingAvailableCount;
+      }
     }
+  }
+
+  setBtnText() {
+    const fullRemainsCardsCount = this.speakerItemElems.length - this.openingCardCount;
+    this.openingAvailableCount = fullRemainsCardsCount >= STEP_OPENED_SPEAKERS ? STEP_OPENED_SPEAKERS : fullRemainsCardsCount;
+    this.btnShowMore.innerHTML = `Показать ещё ${ this.openingAvailableCount }`;
   }
 }
