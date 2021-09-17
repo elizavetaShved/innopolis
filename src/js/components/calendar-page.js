3ea54cbeed89399bfaf7a3befbdd4d3adcef8810
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-09-17 11:14:13 +0300

    временное сохраниние

--- src/js/components/calendar-page.js
+++ src/js/components/calendar-page.js
@@ -1,25 +1,17 @@
 import PerfectScrollbar from 'perfect-scrollbar';
 import Choices from 'choices.js';
-import getDateByDateAttribute from '../functions/getDateByDateAttribute';
+import { CalendarTableComponent } from './calendar-table-component';
 
-const MS_PER_HOUR = 3600000;
 
 export class CalendarPage {
   hostElem;
-  calendarRowElems;
   selectElems;
-  calendarItemElems;
-  calendarHeaderElems;
 
   constructor() {
     this.hostElem = document.querySelector('#calendar-host');
     const calendarContent = this.hostElem.querySelector('.calendar__content');
-    const selectElems = this.hostElem.querySelectorAll('.gl__select');
-    const btnShowAll = this.hostElem.querySelector('.calendar__btn-show-all');
-    this.calendarRowElems = this.hostElem.querySelectorAll('.calendar__row-wrapper');
+    const calendarTableElems = this.hostElem.querySelectorAll('.calendar__table');
     this.selectElems = this.hostElem.querySelectorAll('.gl__select');
-    this.calendarItemElems = Array.from(this.hostElem.querySelectorAll('.calendar__item-content'));
-    this.calendarHeaderElems = Array.from(this.hostElem.querySelectorAll('.calendar__item-header'));
 
     new PerfectScrollbar(calendarContent, {
       wheelSpeed: 1,
@@ -27,7 +19,7 @@ export class CalendarPage {
       minScrollbarLength: 10
     });
 
-    selectElems.forEach(select => {
+    this.selectElems.forEach(select => {
       new Choices(select, {
         searchEnabled: false,
         itemSelectText: '',
@@ -35,105 +27,8 @@ export class CalendarPage {
       })
     })
 
-    this.hideOldRowByTime();
-
-    if (btnShowAll) {
-      btnShowAll.onclick = () => {
-        this.calendarRowElems.forEach(calendarRow => {
-          calendarRow.classList.add('mod-show');
-          btnShowAll.classList.add('mod-hide');
-          btnShowAll.parentElement.classList.add('mod-hide');
-          setTimeout(() => {
-            btnShowAll.parentElement.parentElement.remove();
-          }, 300)
-        })
-      }
-    }
-
-
-    this.onListenFilter();
-  }
-
-  hideOldRowByTime() {
-    const currentTimeMs = new Date().getTime();
-
-    this.calendarRowElems.forEach(calendarRow => {
-      if (calendarRow.getAttribute('data-time')) {
-        const rowTimeMs = getDateByDateAttribute(calendarRow, 'data-time').getTime();
-
-        if (rowTimeMs + MS_PER_HOUR >= currentTimeMs) {
-          calendarRow.classList.add('mod-show');
-        }
-      } else {
-        calendarRow.classList.add('mod-show');
-      }
+    calendarTableElems.forEach(calendarElem => {
+      new CalendarTableComponent(calendarElem, this.selectElems);
     })
-  }
-
-  onListenFilter() {
-    this.selectElems.forEach(select => {
-      if (select.name !== 'commonDates') {
-        select.onchange = () => {
-          switch (select.name) {
-            case 'topics':
-              this.onFilter(select.value, 'topics', 'Тема', '.calendar__item-content-mark');
-              break;
-
-            case 'auditoriums':
-              this.onFilter(select.value, 'auditoriums', 'Аудитория');
-              break;
-
-            case 'speakers':
-              this.onFilter(select.value, 'speakers', 'Спикер', '.calendar__item-content-title');
-              break;
-
-          }
-        }
-      }
-    })
-  }
-
-  onFilter(selectValue, selectName, selectTitle, elemForFilterStr) {
-    if (selectValue !== selectTitle) {
-      this.calendarItemElems.map(elem => elem.classList.remove(`mod-hide-by-${ selectName }`));
-      if (elemForFilterStr) {
-        this.calendarItemElems.forEach(calendarElem => {
-          const elemForFilter = calendarElem.querySelectorAll(elemForFilterStr);
-          elemForFilter.forEach(topic => {
-            if (topic.innerText.toLowerCase() !== selectValue.toLowerCase()) {
-              calendarElem.classList.add(`mod-hide-by-${ selectName }`);
-            }
-          })
-        })
-      } else if (selectName === 'auditoriums') {
-        this.calendarHeaderElems.map(elem => elem.classList.remove(`mod-hide-by-${ selectName }`));
-        let activeColumn;
-        this.calendarHeaderElems.forEach((calendarHeaderElem, indexHeaderElem) => {
-          const calendarHeaderValueElem = calendarHeaderElem.querySelector('.calendar__item-header-value');
-          if (calendarHeaderValueElem) {
-            if (calendarHeaderValueElem.innerText.toLowerCase() !== selectValue.toLowerCase()) {
-              calendarHeaderElem.classList.add(`mod-hide-by-${ selectName }`);
-            } else {
-              activeColumn = indexHeaderElem;
-            }
-          }
-        })
-
-        this.calendarRowElems.forEach(rowElem => {
-          const itemContentForRowElems = Array.from(rowElem.querySelectorAll('.calendar__item-content'));
-          if (itemContentForRowElems.length) {
-            itemContentForRowElems.forEach((elem, i) => {
-
-              if (i !== activeColumn && !elem.className.includes('mod-full') && !elem.className.includes('mod-head')) {
-                elem.classList.add(`mod-hide-by-${ selectName }`)
-              }
-            });
-          }
-        })
-      }
-    } else {
-      this.calendarItemElems.map(elem => elem.classList.remove(`mod-hide-by-${ selectName }`));
-      this.calendarHeaderElems.map(elem => elem.classList.remove(`mod-hide-by-${ selectName }`));
-    }
   }
 }
---
+++ src/js/components/calendar-table-component.js
@@ -0,0 +1,199 @@
+import getDateByDateAttribute from '../functions/getDateByDateAttribute';
+
+const MS_PER_HOUR = 3600000;
+
+export class CalendarTableComponent {
+  hostElem;
+  calendarRowElems;
+  selectElems;
+  calendarHeaderElems;
+  calendarItemElems;
+
+  constructor(hostElem, selectElems) {
+    this.hostElem = hostElem;
+    this.selectElems = selectElems;
+    const btnShowAll = this.hostElem.querySelector('.calendar__btn-show-all');
+    this.calendarRowElems = Array.from(this.hostElem.querySelectorAll('.calendar__row-wrapper'));
+    this.calendarHeaderElems = Array.from(this.hostElem.querySelectorAll('.calendar__item-header:not(.mod-date)'));
+    this.calendarItemElems = Array.from(this.hostElem.querySelectorAll('.calendar__item-content.mod-card'));
+
+    this.hideOldRowByTime();
+
+    if (btnShowAll) {
+      btnShowAll.onclick = () => {
+        this.calendarRowElems.forEach(calendarRow => {
+          calendarRow.classList.add('mod-show');
+          btnShowAll.classList.add('mod-hide');
+          btnShowAll.parentElement.classList.add('mod-hide');
+          setTimeout(() => {
+            btnShowAll.parentElement.parentElement.remove();
+          }, 300)
+        })
+      }
+    }
+
+
+    this.onListenFilter();
+  }
+
+  hideOldRowByTime() {
+    this.calendarRowElems.map(elem => elem.classList.remove('mod-show'));
+
+    const currentTimeMs = new Date().getTime();
+
+    const showRow = (calendarRow) => {
+      calendarRow.classList.add('mod-show');
+    }
+
+    const hideRow = (calendarRow) => {
+      const itemElems = calendarRow.querySelectorAll('.calendar__item-content.mod-card');
+      itemElems.forEach(elem => {
+        elem.classList.add('mod-hide');
+      })
+    }
+
+    this.calendarRowElems.forEach(calendarRow => {
+      if (calendarRow.getAttribute('data-time')) {
+        const rowTimeMs = getDateByDateAttribute(calendarRow, 'data-time').getTime();
+
+        if (rowTimeMs + MS_PER_HOUR >= currentTimeMs) {
+          showRow(calendarRow);
+        } else {
+          hideRow(calendarRow);
+        }
+      } else {
+        showRow(calendarRow);
+      }
+    })
+  }
+
+  onListenFilter() {
+    this.selectElems.forEach(select => {
+      if (select.name !== 'commonDates') {
+        select.onchange = () => {
+          switch (select.name) {
+            case 'topics':
+              this.onFilter(select.value, 'topics', 'Тема', '.calendar__item-content-mark');
+              break;
+
+            case 'auditoriums':
+              this.onFilter(select.value, 'auditoriums', 'Аудитория');
+              break;
+
+            case 'speakers':
+              this.onFilter(select.value, 'speakers', 'Спикер', '.calendar__item-content-title');
+              break;
+
+          }
+        }
+      }
+    })
+  }
+
+  onFilter(selectValue, selectName, selectTitle, elemForFilterStr) {
+    this.calendarRowElems.map(elem => elem.classList.remove('mod-show'));
+    this.hideOldRowByTime();
+
+    if (selectValue !== selectTitle) {
+      this.calendarItemElems.map(elem => elem.classList.remove(`mod-hide-by-${ selectName }`));
+      if (elemForFilterStr) {
+        this.calendarItemElems.forEach(calendarElem => {
+          const elemForFilter = calendarElem.querySelectorAll(elemForFilterStr);
+          elemForFilter.forEach(topic => {
+            if (topic.innerText.toLowerCase() !== selectValue.toLowerCase()) {
+              calendarElem.classList.add(`mod-hide-by-${ selectName }`);
+            }
+          })
+        })
+      } else if (selectName === 'auditoriums') {
+        let activeColumn;
+        this.calendarHeaderElems.forEach((calendarHeaderElem, indexHeaderElem) => {
+          const calendarHeaderValueElem = calendarHeaderElem.querySelector('.calendar__item-header-value');
+          if (calendarHeaderValueElem) {
+            if (calendarHeaderValueElem.innerText.toLowerCase() === selectValue.toLowerCase()) {
+              activeColumn = indexHeaderElem;
+            }
+          }
+        })
+
+        this.calendarRowElems.forEach(rowElem => {
+          const itemContentForRowElems = Array.from(rowElem.querySelectorAll('.calendar__item-content.mod-card:not(.mod-head)'));
+          if (itemContentForRowElems.length) {
+            itemContentForRowElems.forEach((elem, i) => {
+
+              if (i !== activeColumn) {
+                elem.classList.add(`mod-hide-by-${ selectName }`)
+              }
+            });
+          }
+        })
+      }
+
+      // todo сделать убирание пустых элементов
+      // this.hideOldRowByTime()
+      // this.removeRowsWithoutContent();
+      // this.removeColumnWithoutContent();
+
+    } else {
+      this.calendarItemElems.map(elem => elem.classList.remove(`mod-hide-by-${ selectName }`));
+      this.calendarHeaderElems.map(elem => elem.classList.remove(`mod-hide-by-${ selectName }`));
+
+      // this.removeRowsWithoutContent();
+      // this.removeColumnWithoutContent();
+    }
+  }
+
+  removeRowsWithoutContent() {
+    const showCalendarRowElems = this.calendarRowElems.filter(elem => elem.className.includes('mod-show') && !elem.className.includes('mod-head'));
+
+    showCalendarRowElems.forEach(rowElem => {
+      const itemContentForRowElems = rowElem.querySelectorAll('.calendar__item-content.mod-card:not(.mod-empty)');
+      let rowIsShow = false;
+      itemContentForRowElems.forEach(elem => {
+        if (!elem.className.includes('mod-hide')) {
+          rowIsShow = true;
+        }
+      });
+
+      if (!rowIsShow) {
+        rowElem.classList.remove('mod-show');
+
+        const contentEmptyItems = Array.from(this.hostElem.querySelectorAll('.calendar__item-content.mod-card.mod-empty'));
+        contentEmptyItems.map(elem => elem.classList.add('mod-hide'));
+      } else {
+        rowElem.classList.add('mod-show');
+      }
+    })
+  }
+
+  removeColumnWithoutContent() {
+    this.calendarHeaderElems.map(elem => elem.classList.remove('mod-show'));
+
+    const showCalendarRowElems = this.calendarRowElems.filter(elem => elem.className.includes('mod-show'));
+
+    this.calendarHeaderElems.forEach((headerElem, index) => {
+      const columnIsContent = showCalendarRowElems.some(rowElem => {
+        const contentItems = Array.from(rowElem.querySelectorAll('.calendar__item-content.mod-card'));
+        if (contentItems.length && contentItems[index]) {
+          return !contentItems[index].className.includes('mod-hide');
+        } else {
+          return false;
+        }
+      });
+
+      if (!columnIsContent) {
+        this.calendarHeaderElems[index].classList.add('mod-hide');
+        showCalendarRowElems.forEach(rowElem => {
+          const contentItems = Array.from(rowElem.querySelectorAll('.calendar__item-content.mod-card'));
+          if (contentItems.length && contentItems[index]) {
+            contentItems[index].classList.add('mod-hide');
+          }
+        })
+
+
+        const contentEmptyItems = Array.from(this.hostElem.querySelectorAll('.calendar__item-content.mod-card.mod-empty'));
+        contentEmptyItems.map(elem => elem.classList.add('mod-hide'));
+      }
+    })
+  }
+}
