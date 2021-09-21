b3463861da597135253d5edb2f1d8771411789ba
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-09-21 17:08:27 +0300

    date

--- src/js/components/account-page.js
+++ src/js/components/account-page.js
@@ -7,10 +7,26 @@ import { Form } from './common/form';
 export class AccountPage {
   constructor() {
     this.hostElem = document.querySelector('#account-host');
+
+    const universityInputTextWrapper = this.hostElem.querySelector('.account__university');
+    const universityInputText = universityInputTextWrapper.querySelector('.gl__input');
+
+    const universitySelectWrapper = this.hostElem.querySelector('.account__university-type');
+    const universitySelect = universitySelectWrapper.querySelector('.gl__select');
+
     phoneMask();
     new Form(this.hostElem);
     new SelectInit(this.hostElem);
     new Datepicker(this.hostElem);
     new InputFileInit(this.hostElem);
+
+    universityInputText.onchange = () => {
+      if (universityInputText.value) {
+        universitySelectWrapper.classList.add('mod-show');
+      } else {
+        universitySelectWrapper.classList.remove('mod-show');
+        universitySelect.value = null;
+      }
+    }
   }
 }
--- src/js/components/common/datepicker.js
+++ src/js/components/common/datepicker.js
@@ -4,27 +4,27 @@ import 'bootstrap-datepicker/js/locales/bootstrap-datepicker.ru';
 export class Datepicker {
   constructor(hostElem) {
     const datepickerContainerElems = hostElem.querySelectorAll('.gl__datepicker-container');
-    const datepickerInputElems = hostElem.querySelectorAll('.gl__datepicker-input');
+    const datepickerBtnOpenElems = hostElem.querySelectorAll('.gl__input-icon');
 
-    datepickerInputElems.forEach((datepickerInput, index) => {
+    datepickerBtnOpenElems.forEach((datepickerBtn, index) => {
       let endDate;
       let startDate;
       const currentDate = new Date();
 
       switch (true) {
-        case datepickerInput.hasAttribute('data-birth-18'):
+        case datepickerBtn.hasAttribute('data-birth-18'):
           const maxPossibleYear = currentDate.getFullYear() - 18;
           const maxPossibleMouth = currentDate.getMonth();
           const maxPossibleDay = currentDate.getDate() - 1;
           endDate = new Date(maxPossibleYear, maxPossibleMouth, maxPossibleDay);
           break;
 
-        case datepickerInput.hasAttribute('data-future-with-today'):
+        case datepickerBtn.hasAttribute('data-future-with-today'):
           startDate = currentDate;
           break;
       }
 
-      $(datepickerInput)
+      $(datepickerBtn)
         .datepicker({
           format: 'dd.mm.yyyy',
           container: datepickerContainerElems[index],
