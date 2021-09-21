554f263f587c3bf6a50b8280f01303afddbeb3cd
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-09-21 16:02:48 +0300

    filter mobile

--- src/js/components/calendar-page.js
+++ src/js/components/calendar-page.js
@@ -12,6 +12,10 @@ export class CalendarPage {
     const calendarContent = this.hostElem.querySelector('.calendar__content');
     const calendarTableElems = this.hostElem.querySelectorAll('.calendar__table');
 
+    const btnOpenFilter = this.hostElem.querySelector('.calendar__filter-btn');
+    const btnCloseFilter = this.hostElem.querySelector('.calendar__modal-filter-close');
+    const modalFilter = document.querySelector('.calendar__modal-filter');
+
     new PerfectScrollbar(calendarContent, {
       wheelSpeed: 1,
       wheelPropagation: true,
@@ -23,5 +27,13 @@ export class CalendarPage {
     calendarTableElems.forEach(calendarElem => {
       new CalendarTableComponent(calendarElem, this.selectElems);
     })
+
+    btnOpenFilter.onclick = () => {
+      modalFilter.classList.add('mod-show')
+    }
+
+    btnCloseFilter.onclick = () => {
+      modalFilter.classList.remove('mod-show')
+    }
   }
 }
