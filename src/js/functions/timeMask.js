652fec94e78ae71596d6eb4318749410f29b3784
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-10-29 17:37:46 +0300

    time mask 00:00 - 23:59

--- src/js/functions/timeMask.js
+++ src/js/functions/timeMask.js
@@ -3,7 +3,7 @@ import Inputmask from 'inputmask';
 export default function timeMask() {
   const phoneInputs = Array.from(document.querySelectorAll('.js-time-input'));
   phoneInputs.forEach(input => {
-    const instance = new Inputmask({ regex: `^([8-9]|1[0-8]):[0-5][0-9]|[0-0][0-0]$` });
+    const instance = new Inputmask({ regex: `^(([0,1][0-9])|(2[0-3])):[0-5][0-9]$` });
     instance.mask(input);
   });
 }
