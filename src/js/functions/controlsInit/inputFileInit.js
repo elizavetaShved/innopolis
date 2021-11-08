73fcb7d2f73d701307d095590fadf8e69a50c4e2
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-11-08 15:52:41 +0300

    js + css. Стлизация пцр теста

--- src/js/functions/controlsInit/inputFileInit.js
+++ src/js/functions/controlsInit/inputFileInit.js
@@ -6,27 +6,39 @@ export default function inputFileInit() {
   const inputWrapperElems = document.querySelectorAll('.gl__input-wrapper.mod-file');
   const btnClearFile = document.querySelectorAll('.js-btn-clear-file');
 
+  const addStyleFilledFile = (file, index) => {
+    const fileSizeB = file.size;
+    const fileSizeMb = fileSizeB / BYTES_IN_MEGABYTES;
+    const fileSizeKb = fileSizeB / BYTES_IN_KILOBYTES;
+
+    inputWrapperElems[index].classList.add('mod-filled');
+
+    switch (true) {
+      case fileSizeMb >= 1:
+        inputContentElems[index].innerText = `${ file.name } (${ fileSizeMb }Mb)`;
+        break;
+
+      case fileSizeKb >= 1:
+        inputContentElems[index].innerText = `${ file.name } (${ fileSizeKb }Kb)`;
+        break;
+
+      default:
+        inputContentElems[index].innerText = `${ file.name } (${ fileSizeB }B)`;
+    }
+  }
+
   inputFileElems.forEach((inputFile, index) => {
+    const file = inputFile.files[0];
+    if (file) {
+      addStyleFilledFile(file, index);
+    }
+
     inputFile.onchange = () => {
       const file = inputFile.files[0];
       const fileSizeB = file.size;
       const fileSizeMb = fileSizeB / BYTES_IN_MEGABYTES;
-      const fileSizeKb = fileSizeB / BYTES_IN_KILOBYTES;
       if (fileSizeMb <= 2) {
-        inputWrapperElems[index].classList.add('mod-filled');
-
-        switch (true) {
-          case fileSizeMb >=1:
-            inputContentElems[index].innerText = `${ file.name } (${ fileSizeMb }Mb)`;
-            break;
-
-          case fileSizeKb >= 1:
-            inputContentElems[index].innerText = `${ file.name } (${ fileSizeKb }Kb)`;
-            break;
-
-          default:
-            inputContentElems[index].innerText = `${ file.name } (${ fileSizeB }B)`;
-        }
+        addStyleFilledFile(file, index);
       } else {
         console.log('слишком тяжелый файл')
       }
